44f7da69cecaa5eb333e100f0b0821054b8f80e9
Luis <lu15bytes@gmail.com>
2019-03-11 20:01:53 -0600

    CREATED: layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './index.css'
-
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>hola</p>
+        </Layout>
       </div>
     );
   }
